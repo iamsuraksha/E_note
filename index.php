085dbd81785c17858a7a4f431bec05e9c6ecae6c
Suraksha Giri <girisuraksha90@gmail.com>
2022-02-08 21:59:03 +0545

    E-note initilized

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Homepage</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="picture-description.css">
+	<link rel="stylesheet" type="text/css" href="footer.css">
+	<style type="text/css">
+		li{
+			/*background: red;*/
+		}
+		#firstLi{
+			/*background: yellow;*/
+			margin-right: 16px;
+		}
+		#firstLis{
+			/*background: green;*/
+			margin-left: 16px;
+			margin-right: 16px;
+		}
+	</style>
+</head>
+<body bgcolor="#F7F7F7">
+	<div class="nav-bar">
+		<ul>
+			<li id="firstLi"><a href="#">Home</a></li>
+			<li><a href="./aboutPage.html">About</a></li>
+			<li id="firstLis">Contact</li>
+			<li>login</li>
+		</ul>
+	</div>
+	<div class="imageSection">
+		<div style="text-align: center;font-size: 40px; ">
+			Upload your image here !!!
+		</div>
+	</div>
+	<div class="picture-description">
+		<div class="picture">
+			<img src="./karnali.jpg" height="250px">
+		</div>
+		<div class="des">
+			<div style="font-size: 30px;">Title Here</div>
+			<div style="font-size: 20px">Description</div>
+		</div>
+	</div>
+	<div class="footer">
+		<div class="otherFooterContent">
+			<div class="follow-us">Follow Us</div>
+			<div class="profile">Profile</div>
+		</div>
+		<div class="copyrightSection">
+			<span>copyright @ 2021 || Hamro Website</span>
+		</div>
+	</div>
+</body>
+</html>
